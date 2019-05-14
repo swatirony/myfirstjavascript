06993d960004389ddd182e67b947a5ce1ce1a24c
swatirony <50601518+swatirony@users.noreply.github.com>
2019-05-13 20:06:01 -0400

    My first code

---
+++ plunk-swati_may2019/script.js
@@ -0,0 +1,8 @@
+
+console.log('hello world !');
+console.log('my name is swati')
+console.log('i am learning java script');
+console.log('my school');
+console.log('hello plunk');
+console.log('blackjack game');
+console.log('RED TIME'); 
